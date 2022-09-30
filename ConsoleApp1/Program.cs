9a5ea3551cb2d5539f397b84c403b9588025dbb5
jorgeMobando <jorge_obando4@hotmail.com>
2022-09-30 16:48:35 -0500

    primer punto de la prueba tecnica de Sourcecol

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+			int count;
+
+			Console.WriteLine("Ingrese un texto");
+			string cadenaDeCaracteres = Console.ReadLine();
+			//cadenaDeCaracteres = cadenaDeCaracteres.Replace(" ", String.Empty);
+			count = cadenaDeCaracteres.Length;
+
+			if (count > 20)
+			{
+				Console.WriteLine("Los 20 primeros caracteres son: " + cadenaDeCaracteres.Substring(0, 20) + "...");
+			}
+			else
+			{
+				Console.WriteLine($"Los {count} primeros caracteres son: " + cadenaDeCaracteres);
+			}
+
+			Console.ReadKey();
+		}
+	}
+    
+}
